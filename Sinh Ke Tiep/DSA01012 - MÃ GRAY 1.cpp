b4aa54ccbe5dd1f930711b1db58b89af5f4ae837
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-05-24 21:34:59 +0700

    Update DSA01012 - MÃ GRAY 1.cpp

--- Sinh Ke Tiep/DSA01012 - MÃ GRAY 1.cpp
+++ Sinh Ke Tiep/DSA01012 - MÃ GRAY 1.cpp
@@ -36,36 +36,29 @@ void FileIO(){
 // -------------------  Your code STARTS here !!!---------------------------
 
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int t; cin >> t;
-	while(t--){
-		int n;cin >> n;
-		vector <string> str;
-		if(n > 0){
-			str.push_back("0");
-			str.push_back("1");
-			int i, j;
-			for(i = 2; i < (1 << n); i = i << 1){
-				for(j = i - 1; j >= 0; j --){
-					str.push_back(str[j]); 
-				} 
-				for(j = 0; j < i; j ++){
-					str[j] = "0" + str[j]; 
-				} 
-				for(j = i; j < 2 * i; j ++){
-					str[j] = "1" + str[j]; 
-				} 
-			}
-			for(i = 0; i < str.size(); i ++){
-				cout << str[i] << " "; 
-			} 
-			cout << endl; 
-		} 
-	} 
-	return 0;
+int a[100], b[100], n;
+void in() {
+    for (int i = 1; i <= n; ++i) b[i] = a[i];
+    for (int i = 2; i <= n; ++i)
+        b[i] = a[i] ^ a[i - 1];
+    for (int i = 1; i <= n; ++i)
+        cout << b[i];
+    cout << " ";
 }
-
+void Try(int i) {
+    for (int j = 0; j <= 1; ++j) {
+        a[i] = j;
+        if (i == n) in();
+        else Try(i + 1);
+    }
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        Try(1);
+        cout << endl;
+    }
+    return 0;
+}
